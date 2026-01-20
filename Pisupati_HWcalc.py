2e43a43ab42a2e344b60c26055b11cc8fe0c4af1
pisupati <pisupati@Akhilas-MacBook-Pro-824.local>
2026-01-20 06:20:32 -0800

    Updated 302 homework

--- Pisupati_HWcalc.py
+++ Pisupati_HWcalc.py
@@ -1,3 +1,22 @@
 #ASTR 302 HW 1: Calculator Module
 # Akhila A. Pisupati
 # Jan 18, 2026
+
+class Calculator:
+    def add(a, b):
+        c = a + b
+        return c
+    def sub(a,b):
+        c = a - b
+        return c
+    def mul(a,b):
+        c = a*b
+        return c
+    def div(a,b):
+        c = a / b
+        return c
+    def clr():
+        c = 0
+        return c
+
+
