48aba8cddc5a33b24911b0931a60f4dc40c40054
Akhila Anya Pisupati <akhilaanya@users.noreply.github.com>
2026-02-10 10:35:21 -0800

    hw update

--- visutils.py
+++ visutils.py
@@ -8,15 +8,27 @@ def load_and_prepare_cmd(filename):
     data = np.loadtxt("fieldA.csv", delimiter=',', skiprows = 1)
     g_mag = data[:,2] #Taking all of the g mags
     r_mag = data[:,3] #Taking all of the r mags
-    g = np.where((data[:,2] > 14) & (data[:,2] < 24)) #Selecting desired g mags
-    g_minus_r = g_mag - r_mag
-    gr = np.where((g_minus_r > -0.5) & (g_minus_r < 2.5))
+    indexes = np.where((g_mag > 14) & (g_mag < 24)) 
+    g1 = g_mag[indexes] 
+    r1 = r_mag[indexes] 
+    g_minus_r = g1 - r1
+    
+    gr_indexes = np.where((g_minus_r > -0.5) & (g_minus_r < 2.5)) 
+    gr = g_minus_r[gr_indexes]
+    g = g1[gr_indexes]
+    return g, gr
     fieldA = pd.read_csv("fieldA.csv").values
 
-(g, gr) = load_and_prepare_cmd('fieldA.csv')
+(g, gr) = load_and_prepare_cmd("fieldA.csv")
 
-def interactive_hess(g,gr):
-    fig, axs = plt.subplots(
+gridsize_slider = wd.FloatSlider(
+    value = 50,
+    min = 50.0,
+    max = 300.0,
+    step = 1)
+
+def interactive_hess(grid_values):
+    fig, ax = plt.subplots(
         figsize = (15,15),
         constrained_layout = True)
 
@@ -30,24 +42,21 @@ def interactive_hess(g,gr):
                   fontfamily = 'serif',
                   fontsize = 25)
 
+    ax.invert_yaxis()
+
     ax.set_title("302 Homework CMD",
                  fontfamily = 'serif',
                  fontsize = 30)
-
-    ax.plot(g_mag, g_minus_r,
-            color = "purple",
-            marker = "o",
-            linestyle = "None",
-            markersize = 5);
     
-    gridsize_slider = wd.FloatSlider(
-        value = 2.0,
-        min = 50.0,
-        max = 300.0,
-        step = 1,
-        bins = 'log')
+    graph = ax.hexbin(gr, g, gridsize=grid_values, bins = 'log', cmap = 'inferno')
+    bar_color = fig.colorbar(graph, ax=ax)
+    bar_color.set_label('counts')
+    plt.show()
 
-interactive_hess()
+
+plot = wd.interactive(interactive_hess, grid_values = gridsize_slider)
+
+plot
     
     
     
